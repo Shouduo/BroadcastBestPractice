da6a856a88f0e5dc51d849e110ef24f2ae666b94
Shouduo <handsomesaywhynot@gmail.com>
2016-10-09 16:24:21 +0800

    Bug fixed.(Wrong override onCreate() in BaseActivity)

--- app/src/main/java/com/shouduo/broadcastbestpractice/BaseActivity.java
+++ app/src/main/java/com/shouduo/broadcastbestpractice/BaseActivity.java
@@ -1,7 +1,6 @@
 package com.shouduo.broadcastbestpractice;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -10,8 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 public class BaseActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         ActivityCollector.addActivity(this);
     }
 
